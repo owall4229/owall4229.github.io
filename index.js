994fe2fabf19d72b393ee9de51cb76775ea6f8a3
PandaBefriender <84032271+owall4229@users.noreply.github.com>
2025-03-02 12:54:13 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker.js')
+    navigator.serviceWorker.register('/sw.js')
     .then(() => console.log('Service Worker Registered'))
     .catch(err => console.log('Service Worker Registration Failed:', err));
 }
