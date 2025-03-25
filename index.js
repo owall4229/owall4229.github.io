59a040362465846482353cfa1770afab347cdbad
PandaBefriender <84032271+owall4229@users.noreply.github.com>
2025-03-25 17:41:23 -0400

    Delete index.js

--- index.js
+++
@@ -1,5 +0,0 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/sw.js')
-    .then(() => console.log('Service Worker Registered'))
-    .catch(err => console.log('Service Worker Registration Failed:', err));
-}
