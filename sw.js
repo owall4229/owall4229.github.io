620f0842f0672b339855ef9e50f4b8819471838a
PandaBefriender <84032271+owall4229@users.noreply.github.com>
2025-03-25 19:38:18 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = `temperature-converter-v1`;
+const CACHE_NAME = `core-gaming-app`;
     
 // Use the install event to pre-cache all initial resources.
 self.addEventListener('install', event => {
